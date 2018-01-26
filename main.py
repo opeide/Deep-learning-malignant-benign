352fc5a7743a9ddd1790c27f783a9ad825d2b7f6
opeide <fredrikopeide@gmail.com>
2018-01-26 17:43:50 +0100

    testing tensorflow on mnist

--- main.py
+++ main.py
@@ -1,26 +1,77 @@
 import tensorflow as tf
 import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1,1,1,1], padding='SAME')
+
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+
+
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+x = tf.placeholder(tf.float32, shape=[None, 784])
+y_ = tf.placeholder(tf.float32, shape=[None, 10])
+x_img = tf.reshape(x, [-1, 28, 28, 1])
 
 
 
-feature_columns = [tf.feature_column.numeric_column('x', shape=[1])]
-estimator = tf.estimator.LinearRegressor(feature_columns=feature_columns)
+#FIRST LAYER
+W_conv1 = weight_variable([5,5,1,32])
+b_conv1 = bias_variable([32])
+h_conv1 = tf.nn.relu(conv2d(x_img, W_conv1) + b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
 
-x_train = np.array([1.0, 2.0, 3.0, 4.0])
-y_train = np.array([0.0, -1.0, -2.0, -3.0])
-x_eval = np.array([2., 5., 8., 1.])
-y_eval = np.array([-1.01, -4.1, -7, 0.])
+#SECOND LAYER
+W_conv2 = weight_variable([5, 5, 32, 64])
+b_conv2 = bias_variable([64])
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
 
-input_fn = tf.estimator.inputs.numpy_input_fn(
-    {'x':x_train}, y_train, batch_size=4, num_epochs=None, shuffle=True)
-train_input_fn = tf.estimator.inputs.numpy_input_fn(
-    {'x':x_train}, y_train, batch_size=4, num_epochs=1000, shuffle=False)
-eval_input_fn = tf.estimator.inputs.numpy_input_fn(
-    {'x':x_eval}, y_eval, batch_size=4, num_epochs=1000, shuffle=False)
+#FULLY CONNECTED LAYER
+W_fc1 = weight_variable([7*7*64, 1024])
+b_fc1 = bias_variable([1024])
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
-estimator.train(input_fn=input_fn, steps=1000)
+#DROPOUT LAYER
+keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob=keep_prob)
 
-train_metrics = estimator.evaluate(input_fn=train_input_fn)
-eval_metrics = estimator.evaluate(input_fn=eval_input_fn)
-print('train: {}'.format(train_metrics))
-print('eval: {}'.format(eval_metrics))
+#READOUT LAYER
+W_fc2 = weight_variable([1024, 10])
+b_fc2 = bias_variable([10])
+y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+
+
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+correct_predicton = tf.equal(tf.argmax(y_, 1), tf.argmax(y_conv, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_predicton, tf.float32))
+
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+    sess.run(init)
+
+    for i in range(1000):
+        batch = mnist.train.next_batch(50)
+        if i % 100:
+            train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+            print('Step {}: accuracy = {}'.format(i, train_accuracy))
+        train_step.run(feed_dict={x:batch[0], y_:batch[1], keep_prob:0.5})
+
+    train_accuracy = accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})
+    print('FINISHED: accuracy = {}'.format(i, train_accuracy))
